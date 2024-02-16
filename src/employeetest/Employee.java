cf4524037a5a13a6d71b7bcbd374ab6e6f84705e
LHomola <sba23113@student.cct.ie>
2024-02-16 23:05:31 +0000

    add overloaded constructor in Employee.java
    initialises all fields with values passed as parameters.

--- src/employeetest/Employee.java
+++ src/employeetest/Employee.java
@@ -10,4 +10,10 @@ public class Employee {
         this.email = "john.doe@email.com";
         this.empNum = empNum;
     }
+
+    public Employee(String name, String email, int empNum) {
+        this.name = name;
+        this.email = email;
+        this.empNum = empNum;
+    }    
 }
