b696505c38207427d5f2a81b5272037a66de9ac6
LHomola <sba23113@student.cct.ie>
2024-02-16 23:18:37 +0000

    store previously created 3 Employee objects in an array

--- src/employeetest/EmployeeTest.java
+++ src/employeetest/EmployeeTest.java
@@ -6,6 +6,12 @@ public class EmployeeTest {
         Employee employee1 = new  Employee("Joe Bloggs", "jb@gmail.com");
         Employee employee2 = new  Employee("Ann Banana", "ab@gmail.com");
         Employee employee3 = new  Employee("Tom Thumb", "tt@gmail.com");
+        
+        Employee[] projectGroup = new Employee[3];
+        
+        projectGroup[0] = employee1;
+        projectGroup[1] = employee2;
+        projectGroup[2] = employee3;
     }
     
 }
