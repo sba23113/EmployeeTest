57398cc37b193f971e24d49be5bb9bc3849d1ad9
LHomola <sba23113@student.cct.ie>
2024-02-16 23:04:33 +0000

    add constructor in Employee.java
    initialises the name and email instance fields with default values

--- src/employeetest/Employee.java
+++ src/employeetest/Employee.java
@@ -4,4 +4,10 @@ public class Employee {
     private String name;
     private String email;
     private int empNum;
+
+    public Employee(int empNum) {
+        this.name = "John Doe";
+        this.email = "john.doe@email.com";
+        this.empNum = empNum;
+    }
 }
