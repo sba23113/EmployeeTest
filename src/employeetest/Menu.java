b11aea25a24c1bf5964c0e43ca302bf808544860
LHomola <sba23113@student.cct.ie>
2024-02-17 21:41:26 +0000

    add fields and default constructor in Menu class
    private Scanner scanner;
    private Company company;
    private Manager manager;

--- src/employeetest/Menu.java
+++ src/employeetest/Menu.java
@@ -1,5 +1,15 @@
 package employeetest;
 
+import java.util.Scanner;
+
 public class Menu {
-    
+    private Scanner scanner;
+    private Company company;
+    private Manager manager;
+  
+    public Menu(Company company, Manager manager) {
+        this.scanner = new Scanner(System.in);
+        this.company = company;
+        this.manager = manager;
+    }
 }
