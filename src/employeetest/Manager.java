92dc7d9f31ddac874a2f203f99f23c1b383fa37f
LHomola <sba23113@student.cct.ie>
2024-02-17 13:48:34 +0000

    add getters and setters in Manager class

--- src/employeetest/Manager.java
+++ src/employeetest/Manager.java
@@ -9,6 +9,20 @@ public class Manager extends Employee {
         this.username = username;
         this.password = password;
     }
-    
-    
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
