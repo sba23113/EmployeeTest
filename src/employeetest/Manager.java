50a922245a7ebe49ed4c4c30cffd33249f40700d
LHomola <sba23113@student.cct.ie>
2024-02-17 13:46:14 +0000

    add default constructor to Manager class
    accepts all values as a parameter

--- src/employeetest/Manager.java
+++ src/employeetest/Manager.java
@@ -3,4 +3,12 @@ package employeetest;
 public class Manager extends Employee {
     private String username;
     private String password;
+
+    public Manager(String username, String password, String name, String email) {
+        super(name, email);
+        this.username = username;
+        this.password = password;
+    }
+    
+    
 }
