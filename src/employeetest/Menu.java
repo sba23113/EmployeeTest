0ec05442ddf4b499d3a1afda90e14c5a8e3ea625
LHomola <sba23113@student.cct.ie>
2024-02-18 11:27:01 +0000

    move try catch for user input into getIntInput method
    Menu class

--- src/employeetest/Menu.java
+++ src/employeetest/Menu.java
@@ -16,67 +16,63 @@ public class Menu {
     
     public void displayMenu() {
         while (true) {
-            try {
-                System.out.println("****************************************************************************");
-                System.out.println("Employee Management System for People Managers");
-                System.out.println("****************************************************************************");
-                System.out.println("1) Login");
-                System.out.println("0) Exit");
-                System.out.println("");
-                System.out.print("Enter your choice: ");
-                int choice = getIntInput();
+            System.out.println("****************************************************************************");
+            System.out.println("Employee Management System for People Managers");
+            System.out.println("****************************************************************************");
+            System.out.println("1) Login");
+            System.out.println("0) Exit");
+            System.out.println("");
+            System.out.print("Enter your choice: ");
+            int choice = getIntInput();
 
-                if (choice == 1) {
-                    System.out.print("Enter username: ");
-                    String username = scanner.nextLine();
-                    System.out.print("Enter password: ");
-                    String password = scanner.nextLine();
-                    if (username.equals(manager.getUsername()) && password.equals(manager.getPassword())) {
+            if (choice == 1) {
+                System.out.print("Enter username: ");
+                String username = scanner.nextLine();
+                System.out.print("Enter password: ");
+                String password = scanner.nextLine();
+                
+                if (username.equals(manager.getUsername()) && password.equals(manager.getPassword())) {
+                    System.out.println("");
+                    System.out.printf("Welcome %s! \n", manager.getName());
+                    
+                    while (true) {
                         System.out.println("");
-                        System.out.printf("Welcome %s! \n", manager.getName());
-                        while (true) {
-                            System.out.println("");
-                            System.out.println("****************************************************************************");
-                            System.out.printf("Staff Administration (Logged in as '%s') \n", username);
-                            System.out.println("****************************************************************************");
-                            System.out.println("1) View staff");
-                            System.out.println("2) Add new member of staff");
-                            System.out.println("3) Remove member of staff");
-                            System.out.println("0) Logout");
-                            System.out.println("");
-                            System.out.print("Enter your choice: ");
-                            int managerChoice = getIntInput();
+                        System.out.println("****************************************************************************");
+                        System.out.printf("Staff Administration (Logged in as '%s') \n", username);
+                        System.out.println("****************************************************************************");
+                        System.out.println("1) View staff");
+                        System.out.println("2) Add new member of staff");
+                        System.out.println("3) Remove member of staff");
+                        System.out.println("0) Logout");
+                        System.out.println("");
+                        System.out.print("Enter your choice: ");
+                        int managerChoice = getIntInput();
 
-                            switch (managerChoice) {
-                                case 1:
-                                    System.out.println("***Current employees***");
-                                    company.listEmployees(0);
-                                    break;
-                                case 2:
-                                    addNewStaff();
-                                    break;
-                                case 3:
-                                    removeStaff();
-                                    break;
-                                case 0:
-                                    break;
-                                default:
-                                    System.out.println("Invalid choice");
-                                    break;
-                            }
+                        switch (managerChoice) {
+                            case 1:
+                                System.out.println("***Current employees***");
+                                company.listEmployees(0);
+                                break;
+                            case 2:
+                                addNewStaff();
+                                break;
+                            case 3:
+                                removeStaff();
+                                break;
+                            case 0:
+                                break;
+                            default:
+                                System.out.println("Invalid choice");
+                                break;
                         }
-                    } else {
-                        System.out.println("");
-                        System.out.println("!! Invalid username or password !!");
                     }
-                } else if (choice == 0) {
-                    break;
+                } else {
+                    System.out.println("");
+                    System.out.println("!! Invalid username or password !!");
                 }
-            } catch (InputMismatchException e) {
-                System.out.println("Invalid input. Please enter a number.");
-                 // Consume the invalid input
-                scanner.nextLine();
-            }
+            } else if (choice == 0) {
+                break;
+            }            
         }
     }
     
@@ -97,7 +93,9 @@ public class Menu {
         System.out.println("***Remove employee from database***");
         System.out.print("Enter ID of employee to be removed: ");
         int empNum = getIntInput();
+        
         boolean isRemoved = company.removeStaff(empNum);
+        
         if (isRemoved) {
             System.out.printf("Employee ID '%d' removed from employee database.\n", empNum);
         } else {
@@ -106,9 +104,17 @@ public class Menu {
     }
     
     private int getIntInput() {
-        int input = scanner.nextInt();
-        // consume any newline characters left in scanner buffer
-        scanner.nextLine();
-        return input;
+        while (true) {
+            try {
+                int input = scanner.nextInt();
+                // consume any newline characters left in scanner buffer
+                scanner.nextLine();
+                return input;
+            } catch (InputMismatchException e) {
+                System.out.println("Invalid input. Please enter a number!");
+                // Consume the invalid input
+                scanner.nextLine();
+            }
+        }
     }
 }
