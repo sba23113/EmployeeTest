7fc27aa8a194957b89b09c012b3776320918421f
LHomola <sba23113@student.cct.ie>
2024-02-18 10:30:11 +0000

    add try catch for user input in login menu in Menu class

--- src/employeetest/Menu.java
+++ src/employeetest/Menu.java
@@ -1,5 +1,6 @@
 package employeetest;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Menu {
@@ -15,75 +16,82 @@ public class Menu {
     
     public void displayMenu() {
         while (true) {
-            System.out.println("****************************************************************************");
-            System.out.println("Employee Management System for People Managers");
-            System.out.println("****************************************************************************");
-            System.out.println("1) Login");
-            System.out.println("0) Exit");
-            System.out.println("");
-            System.out.print("Enter your choice: ");
-            int choice = scanner.nextInt();
-            // consume any newline characters left in scanner buffer
-            scanner.nextLine();
+            try {
+                System.out.println("****************************************************************************");
+                System.out.println("Employee Management System for People Managers");
+                System.out.println("****************************************************************************");
+                System.out.println("1) Login");
+                System.out.println("0) Exit");
+                System.out.println("");
+                System.out.print("Enter your choice: ");
+                int choice = scanner.nextInt();
+                // consume any newline characters left in scanner buffer
+                scanner.nextLine();
 
-            if (choice == 1) {
-                System.out.print("Enter username: ");
-                String username = scanner.nextLine();
-                System.out.print("Enter password: ");
-                String password = scanner.nextLine();
-                if (username.equals(manager.getUsername()) && password.equals(manager.getPassword())) {
-                    System.out.println("");
-                    System.out.printf("Welcome %s! \n", manager.getName());
-                    while (true) {
+                if (choice == 1) {
+                    System.out.print("Enter username: ");
+                    String username = scanner.nextLine();
+                    System.out.print("Enter password: ");
+                    String password = scanner.nextLine();
+                    if (username.equals(manager.getUsername()) && password.equals(manager.getPassword())) {
                         System.out.println("");
-                        System.out.println("****************************************************************************");
-                        System.out.printf("Staff Administration (Logged in as '%s') \n", username);
-                        System.out.println("****************************************************************************");
-                        System.out.println("1) View staff");
-                        System.out.println("2) Add new member of staff");
-                        System.out.println("3) Remove member of staff");
-                        System.out.println("0) Logout");
-                        System.out.println("");
-                        System.out.print("Enter your choice: ");
-                        int managerChoice = scanner.nextInt();
-                        System.out.println("");
-                        // consume any newline characters left in scanner buffer
-                        scanner.nextLine();
+                        System.out.printf("Welcome %s! \n", manager.getName());
+                        while (true) {
+                            System.out.println("");
+                            System.out.println("****************************************************************************");
+                            System.out.printf("Staff Administration (Logged in as '%s') \n", username);
+                            System.out.println("****************************************************************************");
+                            System.out.println("1) View staff");
+                            System.out.println("2) Add new member of staff");
+                            System.out.println("3) Remove member of staff");
+                            System.out.println("0) Logout");
+                            System.out.println("");
+                            System.out.print("Enter your choice: ");
+                            int managerChoice = scanner.nextInt();
+                            System.out.println("");
+                            // consume any newline characters left in scanner buffer
+                            scanner.nextLine();
 
-                        if (managerChoice == 1) {
-                            System.out.println("***Current employees***");
-                            company.listEmployees(0);
-                        } else if (managerChoice == 2) {
-                            System.out.println("***Add new employee details***");
-                            System.out.print("Enter employee name: ");
-                            String name = scanner.nextLine();
-                            System.out.print("Enter employee email: ");
-                            String email = scanner.nextLine();
-                            Employee newEmp = new Employee(name, email);
-                            company.addNewStaff(newEmp);
-                            System.out.printf("%s added to employee database." , name);
-                            System.out.println("");
-                        } else if (managerChoice == 3) {
-                            System.out.println("***Remove employee from database***");
-                            System.out.print("Enter ID of employee to be removed: ");
-                            int empNum = scanner.nextInt();
-                            boolean isRemoved = company.removeStaff(empNum);
-                            if (isRemoved) {
-                                System.out.printf("Employee ID '%d' removed from employee database." , empNum);
-                            } else {
-                                System.out.printf("Employee ID '%d' not found." , empNum);
-                            }                            
-                            System.out.println("");
-                        } else if (managerChoice == 0) {
-                            break;
+    // switch satement here instead
+                            if (managerChoice == 1) {
+                                System.out.println("***Current employees***");
+                                company.listEmployees(0);
+                            } else if (managerChoice == 2) {
+                                System.out.println("***Add new employee details***");
+                                System.out.print("Enter employee name: ");
+                                String name = scanner.nextLine();
+                                System.out.print("Enter employee email: ");
+                                String email = scanner.nextLine();
+                                Employee newEmp = new Employee(name, email);
+                                company.addNewStaff(newEmp);
+                                System.out.printf("%s added to employee database." , name);
+                                System.out.println("");
+                            } else if (managerChoice == 3) {
+                                System.out.println("***Remove employee from database***");
+                                System.out.print("Enter ID of employee to be removed: ");
+                                int empNum = scanner.nextInt();
+                                boolean isRemoved = company.removeStaff(empNum);
+                                if (isRemoved) {
+                                    System.out.printf("Employee ID '%d' removed from employee database." , empNum);
+                                } else {
+                                    System.out.printf("Employee ID '%d' not found." , empNum);
+                                }                            
+                                System.out.println("");
+                            } else if (managerChoice == 0) {
+                                break;
+                            }
                         }
+                    } else {
+                        System.out.println("");
+                        System.out.println("!! Invalid username or password !!");
                     }
-                } else {
-                    System.out.println("");
-                    System.out.println("!! Invalid username or password !!");
+                } else if (choice == 0) {
+                    break;
                 }
-            } else if (choice == 0) {
-                break;
+            } catch (InputMismatchException e) {
+                System.out.println("Invalid input. Please enter a number.");
+                 // Consume the invalid input
+                scanner.nextLine();
             }
         }
     }
